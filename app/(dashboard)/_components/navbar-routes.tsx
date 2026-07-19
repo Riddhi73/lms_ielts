d6574bd23b089098619d8dd919b94e8ac3ae874e
Riddhi73 <riddhimandebnath4260@gmail.com>
2026-07-19 21:48:43 +0600

    Add search query

--- app/(dashboard)/_components/navbar-routes.tsx
+++ app/(dashboard)/_components/navbar-routes.tsx
@@ -5,30 +5,38 @@ import { UserButton } from "@clerk/nextjs";
 import { LogOut } from "lucide-react";
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
+import { SearchInput } from "./search-input";
 
 export const NavbarRoutes = () => {
   const pathName = usePathname();
   const router = useRouter();
   const isTeacherPage = pathName?.startsWith("/teacher");
   const isPlayerPage = pathName?.includes("/chapter");
-
+  const isSearchPage = pathName === "/search";
   return (
-    <div className="flex gap-x-2 ml-auto">
-      {isTeacherPage || isPlayerPage ? (
-        <Link href="/">
-          <Button size="sm" variant="ghost">
-            <LogOut className="h-4 w-4 mr-2" />
-            Exit
-          </Button>
-        </Link>
-      ) : (
-        <Link href="/teacher/courses">
-          <Button size="sm" variant="ghost">
-            Teacher Mode
-          </Button>
-        </Link>
+    <>
+      {isSearchPage && (
+        <div className="hidden md:block">
+          <SearchInput />
+        </div>
       )}
-      <UserButton />
-    </div>
+      <div className="flex gap-x-2 ml-auto">
+        {isTeacherPage || isPlayerPage ? (
+          <Link href="/">
+            <Button size="sm" variant="ghost">
+              <LogOut className="h-4 w-4 mr-2" />
+              Exit
+            </Button>
+          </Link>
+        ) : (
+          <Link href="/teacher/courses">
+            <Button size="sm" variant="ghost">
+              Teacher Mode
+            </Button>
+          </Link>
+        )}
+        <UserButton />
+      </div>
+    </>
   );
 };
