f3fb7245568658dda5dca1e6ece47369ce4ba0f5
Riddhi73 <riddhimandebnath4260@gmail.com>
2026-07-23 00:28:51 +0600

    Reverse changes

--- actions/get-analytics.ts
+++ actions/get-analytics.ts
@@ -1,5 +1,5 @@
 import { db } from "@/lib/db";
-import type { Course, Purchase } from "@prisma/client";
+import { Course, Purchase } from "@prisma/client";
 
 type PurchaseWithCourse = Purchase & {
   course: Course;
